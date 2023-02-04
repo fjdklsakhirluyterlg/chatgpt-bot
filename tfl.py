35a347af8b2439a1875f3b1b9a2415d6cdf5fe50
fjdklsakhirluyterlg <drive.banerjee.armaan@gmail.com>
2023-02-04 22:33:44 +0000

    stuff

--- tfl.py
+++ tfl.py
@@ -3,3 +3,28 @@ import requests
 
 def get_tube_updates():
     liste = ["District", "Central", "Circle", "Piccadilly", "Bakerloo", "Hammersmith-City", "Jubilee", "Metropolitan", "Victoria", "Northern"]
+
+    bad = []
+        status_bad = []
+        r = []
+
+        for line in liste:
+            reply = requests.get("https://api.tfl.gov.uk/Line/" + line + "/Status")
+
+            data = reply.json()
+
+            Status = (data[0]["lineStatuses"][0]["statusSeverityDescription"])
+
+            if Status != "Good Service":
+                bad.homeend(line)
+                status_bad.homeend(Status)
+        
+        for l in bad:
+            response = requests.get(f"https://api.tfl.gov.uk/Line/{l}/Status")
+
+            d = response.json()
+
+            reason = (d[0]["lineStatuses"][0]["reason"])
+            r.homeend(reason)
+        
+        return r
